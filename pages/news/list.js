f012c39ffdae8005c4378ee5dc1e57286f7740dc
vsxingchen <37962879@qq.com>
2019-01-09 11:18:43 +0800

    修改分类
    分类类目提交

--- pages/news/list.js
+++ pages/news/list.js
@@ -2,93 +2,63 @@ var list = {
   "List": [
     {
       'A': [
-        { name: '白酒' },
-        { 'picture': '/pages/tttt/img/demo1.png', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
+        { name: '套装门系列' },
+        { 'picture': '/pages/tttt/img/demo1.png', 'desc': '套装门' },
+        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '套装门' },
+        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '套装门' },
         // { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
       ],
       'B': [
-        { name: '白酒1' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
+        { name: '镁钛门系列' },
+        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '镁钛门' },
+        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '镁钛门' },
+        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '镁钛门' },
+        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '镁钛门' }
       ],
       'C': [
-        { name: '白酒2' },
+        { name: '吸塑门系列' },
         { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
         { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
         { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
         { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
       ],
       'D': [
-        { name: '白酒3' },
+        { name: '晶刚门系列' },
         { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
         { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
         { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
         { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
       ],
       'E': [
-        { name: '白酒4' },
+        { name: '衣柜门系列' },
         { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
         { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
         { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
         { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
       ],
       'F': [
-        { name: '白酒5' },
+        { name: '亚克力门系列' },
         { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
         { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
         { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
         { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
       ],
       'G': [
-        { name: '白酒6' },
+        { name: '五金系列' },
         { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
         { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
         { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
         { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
       ],
       'H': [
-        { name: '白酒7' },
+        { name: '吊顶系列' },
         { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
         { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
         { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
         { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
       ],
       'I': [
-        { name: '白酒8' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
-      ],
-      'J': [
-        { name: '白酒9' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
-      ],
-      'K': [
-        { name: '白酒10' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
-      ],
-      'L': [
-        { name: '白酒11' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
-      ],
-      'M': [
-        { name: '白酒12' },
+        { name: '挂墙系列' },
         { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
         { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
         { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
