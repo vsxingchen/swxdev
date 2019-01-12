3f6f0bb47bb1aa5c2b54d58a60c78a3c313488de
vsxingchen <37962879@qq.com>
2019-01-12 14:17:04 +0800

    产品中心增加图片

--- pages/decoration/index.js
+++ pages/decoration/index.js
@@ -1,99 +1,24 @@
-var list = require('list.js')
-Page({
-  data: {
-    // 左侧点击类样式
-    curNav: 'A',
-    scrollTop: 0,
-    // 定义一个空数组，用来存放右侧滑栏中每一个商品分类的 Height
-    listHeight: ''
-  },
-  // 生命周期函数--监听页面初次渲染完成
-  onReady: function () {
-    var that = this;
-    // 定义右侧标题的 rpx 高度 和 px 高度
-    var right_titleRpxHeight = 60;
-    var right_titleHeight;
-    // 定义右侧单个商品的 rpx 高度 和 px 高度
-    var right_contentRpxHeight = 180;
-    var right_contentHeight;
-    // 定义左侧单个tab的 rpx 高度 和 px 高度
-    var left_titleRpxHeight = 140;
-    var left_titleHeight;
-    //  获取可视区屏幕高度
-    wx.getSystemInfo({
-      success: function (res) {
-        // percent 为当前设备1rpx对应的px值
-        var percent = res.windowWidth / 750;
-        that.setData({
-          winHeight: res.windowHeight,
-          right_titleHeight: Number(right_titleRpxHeight * percent),
-          right_contentHeight: Number(right_contentRpxHeight * percent),
-          left_titleHeight: Number(left_titleRpxHeight * percent)
-        })
-      }
-    })
-    // 把请求到的 list 中的数据赋值给  listChild1
-    var listChild1 = list.List[0];
-    // 定义一个 names ，用于存放 scroll-into-view 使用的 id
-    var names = '';
-    // 循环 listChild1 中的每一项
-    for (var item in listChild1) {
-      // 把 listChild1 中每一项的键值用“：”（便于后期处理）分隔开，存入 names 中，数据格式见图‘names中的数据’
-      names += ":" + item;
-      // 计算右侧每一个分类的 Height 。
-      // listChild1 下的每一个 item 中包含该分类的 title，所以 listChild1[item].length 需要减一 
-      // 右侧每一个分类中每一行放两个商品，所以 this.data.right_contentHeight 除二
-      // 最后加上 right_titleHeight，此时 height 为右侧一个完整分类的高度
-      var height = (listChild1[item].length - 1) * this.data.right_contentHeight / 2 + this.data.right_titleHeight;
-      // 同上面 names 的道理，把每一个 height 用“：”隔开放入 listHeight 中
-      this.data.listHeight += ":" + height;
-      this.setData({
-        // 把 listChild1 赋值给 list ，供 wxml 中循环使用
-        list: listChild1,
-        listHeight: this.data.listHeight
-      })
-    }
-    // 把 names 的数据切成数组
-    var names = names.substring(1).split(':');
-    this.setData({
-      names: names
-    })
-  },
-  // 右侧滑栏的 bindscroll 事件函数（ES6写法）
-  scroll(event) {
-    // 把 listHeight 切割成数组
-    var height = this.data.listHeight.substring(1).split(':');
-    // 定义一个 index 供左侧边栏联动使用
-    var index = 1;
-    var num = 0;
-    for (var i = 0; i < height.length; i++) {
-      // 累计右侧滑栏滚动上去的每一个分类的 Height
-      num += parseInt(height[i]);
-      // 循环判断 num 是否大于右侧滑栏滚动上去的 Height ，然后 get 到 i 值赋给 index
-      if (num > event.detail.scrollTop) {
-        index = i + 1;
-        // 如果右侧滑栏滚动高度小于单个类别高度的 1/2 时，index 为 0
-        if (event.detail.scrollTop < height[0] / 2) {
-          index = 0;
-        }
-        break;
-      }
-    }
-    // 定义并设置左侧边栏的滚动高度
-    var left_scrollTop = this.data.left_titleHeight * index
-    this.setData({
-      scrollTop: left_scrollTop,
-      // 动态给左侧滑栏传递对应该项的 id，用于高亮效果显示
-      curNav: this.data.names[index]
-    })
-  },
-  //点击左侧 tab ，右侧列表相应位置联动 置顶
-  switchRightTab: function (e) {
-    var id = e.target.id;
-    this.setData({
-      scrollTopId: id,
-      // 左侧点击类样式
-      curNav: id,
-    })
-  }
+/*index.js*/
+//获取应用实例
+const app = getApp()
+var util = require('../../utils/util.js');
+Page({  
+  data: {    
+    imgalist: [{ "barCode": "", "categoryId": 6218, "characteristic": "1.5~2.8万元，拎包入住", "commission": 0.00, "commissionType": 0, "dateAdd": "2018-01-18 13:52:11", "dateStart": "2018-01-18 13:51:53", "dateUpdate": "2018-01-18 13:52:19", "gotScore": 0, "gotScoreType": 0, "id": 19132, "kanjia": false, "kanjiaPrice": 0.00, "logisticsId": 0, "minPrice": 0.00, "minScore": 0, "name": "出租房", "numberFav": 0, "numberGoodReputation": 0, "numberOrders": 0, "numberSells": 0, "originalPrice": 0.00, "paixu": 0, "pic": "cloud://wj-38d226.776a-wj-38d226/chuzu.jpg", "pingtuan": false, "pingtuanPrice": 0.00, "recommendStatus": 0, "recommendStatusStr": "普通", "shopId": 0, "status": 0, "statusStr": "上架", "stores": 0, "userId": 3387, "videoId": "", "views": 0, "weight": 0.00 }, { "barCode": "", "categoryId": 6218, "characteristic": "3.9~9.8万，简单拎包入住", "commission": 0.00, "commissionType": 0, "dateAdd": "2018-01-18 13:51:43", "dateStart": "2018-01-18 13:51:23", "dateUpdate": "2018-01-18 13:51:50", "gotScore": 0, "gotScoreType": 0, "id": 19131, "kanjia": false, "kanjiaPrice": 0.00, "logisticsId": 0, "minPrice": 0.00, "minScore": 0, "name": "简单装修", "numberFav": 0, "numberGoodReputation": 0, "numberOrders": 0, "numberSells": 0, "originalPrice": 0.00, "paixu": 0, "pic": "cloud://wj-38d226.776a-wj-38d226/jianzhuang.jpg", "pingtuan": false, "pingtuanPrice": 0.00, "recommendStatus": 0, "recommendStatusStr": "普通", "shopId": 0, "status": 0, "statusStr": "上架", "stores": 0, "userId": 3387, "videoId": "", "views": 0, "weight": 0.00 }, { "barCode": "", "categoryId": 6218, "characteristic": "精装9.8~35万，拎包入住", "commission": 0.00, "commissionType": 0, "dateAdd": "2018-01-18 13:51:06", "dateStart": "2018-01-18 13:50:49", "gotScore": 0, "gotScoreType": 0, "id": 19130, "kanjia": false, "kanjiaPrice": 0.00, "logisticsId": 0, "minPrice": 0.00, "minScore": 0, "name": "豪华装修", "numberFav": 0, "numberGoodReputation": 0, "numberOrders": 0, "numberSells": 0, "originalPrice": 0.00, "paixu": 0, "pic": "cloud://wj-38d226.776a-wj-38d226/jingzhuang.jpg", "pingtuan": false, "pingtuanPrice": 0.00, "recommendStatus": 0, "recommendStatusStr": "普通", "shopId": 0, "status": 0, "statusStr": "上架", "stores": 0, "userId": 3387, "videoId": "", "views": 0, "weight": 0.00 }]
+	},
+	onLoad: function(){
+		var _this = this;
+		// 案例列表获取
+	    util.requestFun({
+	    //  url: 'shop/goods/list?categoryId=6218&pageSize=6',
+	      success: function(res){
+	        _this.setData({
+	         // imgalist : res.data.data
+	        })
+	      }
+	    })
+	},
+	showImages:function(event){
+    app.showImages(event, this.data.imgalist)
+  } 
 })
--- pages/news/list.js
+++ pages/news/list.js
@@ -3,66 +3,52 @@ var list = {
     {
       'A': [
         { name: '套装门系列' },
-        { 'picture': '/pages/tttt/img/demo1.png', 'desc': '套装门' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '套装门' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '套装门' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/taozhuangmen1.jpg', 'desc': '套装门' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/taozhuangmen2.jpg', 'desc': '套装门' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/taozhuangmen3.jpg', 'desc': '套装门' },
         // { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
       ],
       'B': [
         { name: '镁钛门系列' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '镁钛门' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '镁钛门' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '镁钛门' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '镁钛门' }
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/meitaimen1.jpg', 'desc': '镁钛门' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/meitaimen2.jpg', 'desc': '镁钛门' }
       ],
       'C': [
         { name: '吸塑门系列' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/xishumen1.jpg', 'desc': '吸塑门' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/xishumen2.jpg', 'desc': '吸塑门' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/xishumen3.jpg', 'desc': '吸塑门' }
       ],
       'D': [
-        { name: '晶刚门系列' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
+        { name: '晶钢门系列' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/jingangmen1.jpg', 'desc': '晶钢门' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/jingangmen2.jpg', 'desc': '晶钢门' }
       ],
       'E': [
         { name: '衣柜门系列' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/yiguimen1.jpg', 'desc': '衣柜门' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/yiguimen2.jpg', 'desc': '衣柜门' }
       ],
       'F': [
         { name: '亚克力门系列' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/yakeli1.jpg', 'desc': '亚克力门' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/yakeli2.jpg', 'desc': '亚克力门' }
       ],
       'G': [
         { name: '五金系列' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/wujin1.jpg', 'desc': '五金' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/wujin2.jpg', 'desc': '五金' }
       ],
       'H': [
         { name: '吊顶系列' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/diaoding1.jpg', 'desc': '吊顶' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/diaoding2.jpg', 'desc': '吊顶' }
       ],
       'I': [
-        { name: '挂墙系列' },
-        { 'picture': '../../img/55ac9689Ncc876cf1.jpg', 'desc': '葡萄酒' },
-        { 'picture': '../../img/56668c04N5cb325b7.jpg', 'desc': '洋酒' },
-        { 'picture': '../../img/586e055eNf678fd52.png', 'desc': '汾酒' },
-        { 'picture': '../../img/596d6f4eNb62c24c1.jpg', 'desc': '汾酒' }
+        { name: '刮墙系列' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/guaqiang1.jpg', 'desc': '刮墙' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/guaqiang2.jpg', 'desc': '刮墙' },
+        { 'picture': 'cloud://wj-38d226.776a-wj-38d226/guaqiang3.jpg', 'desc': '刮墙' }
       ]
     }
   ],
